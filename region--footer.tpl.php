d078ec0b3cf7fb2b63026bff2d570bf50b28ecc0
ChemaSalazar <chemasalazar@outlook.com>
2019-02-06 02:33:42 -0600

    :raised_hands: column footers 1-4 added to template, vertical tab form style added for usability, footer column style form defined and attached to template file.

---
+++ region--footer.tpl.php
@@ -0,0 +1,218 @@
+<!-- Begining of the footer region -->
+<footer>
+<!-- If this style is enabled on site information, then use this style -->
+<!-- The $footer_style variable will be used on the site information section as a checkbox or an option. -->
+
+<div id="footer-container">
+
+
+<!-- This is the 4-col style-->
+<?php if ($footer_style == "4"):
+
+  <!-- First footer column -->
+  <div class="col-sm-12 col-lg-3">
+    <ol>
+      <li>
+        <a href="<?php echo variable_get('c_1_1') ? >" target="_blank" > C-1-1 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_2') ? >" target="_blank" > C-1-2 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_3') ? >" target="_blank" > C-1-3 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_4') ? >" target="_blank" > C-1-4 </a>
+      </li>
+    </ol>
+  </div>
+
+  <!-- Second footer column -->
+  <div class="col-sm-12 col-lg-3">
+    <ol>
+      <li>
+        <a href="<?php echo variable_get('c_2_1') ? >" target="_blank" > C-2-1 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_2_2') ? >" target="_blank" > C-2-2 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_2_3') ? >" target="_blank" > C-2-3 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_2_4') ? >" target="_blank" > C-2-4 </a>
+      </li>
+    </ol>
+  </div>
+
+  <!-- Third footer column -->
+  <div class="col-sm-12 col-lg-3">
+    <ol>
+      <li>
+        <a href="<?php echo variable_get('c_3_1') ? >" target="_blank" > C-3-1 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_3_2') ? >" target="_blank" > C-3-2 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_3_3') ? >" target="_blank" > C-3-3 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_3_4') ? >" target="_blank" > C-3-4 </a>
+      </li>
+    </ol>
+  </div>
+
+  <!-- Fourth footer column -->
+  <div class="col-sm-12 col-lg-3">
+    <ol>
+      <li>
+        <a href="<?php echo variable_get('c_4_1') ? >" target="_blank" > C-4-1 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_4_2') ? >" target="_blank" > C-4-2 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_4_3') ? >" target="_blank" > C-4-3 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_4_4') ? >" target="_blank" > C-4-4 </a>
+      </li>
+    </ol>
+  </div>
+
+?>
+
+
+<!-- This is the 3- column style -->
+<?php if ($footer_style == "3"):
+
+  <!-- First footer column -->
+  <div class="col-sm-12 col-lg-3">
+    <ol>
+      <li>
+        <a href="<?php echo variable_get('c_1_1') ? >" target="_blank" > C-1-1 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_2') ? >" target="_blank" > C-1-2 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_3') ? >" target="_blank" > C-1-3 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_4') ? >" target="_blank" > C-1-4 </a>
+      </li>
+    </ol>
+  </div>
+
+  <!-- Second footer column -->
+  <div class="col-sm-12 col-lg-3">
+    <ol>
+      <li>
+        <a href="<?php echo variable_get('c_2_1') ? >" target="_blank" > C-2-1 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_2_2') ? >" target="_blank" > C-2-2 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_2_3') ? >" target="_blank" > C-2-3 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_2_4') ? >" target="_blank" > C-2-4 </a>
+      </li>
+    </ol>
+  </div>
+
+  <!-- Third footer column -->
+  <div class="col-sm-12 col-lg-3">
+    <ol>
+      <li>
+        <a href="<?php echo variable_get('c_3_1') ? >" target="_blank" > C-3-1 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_3_2') ? >" target="_blank" > C-3-2 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_3_3') ? >" target="_blank" > C-3-3 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_3_4') ? >" target="_blank" > C-3-4 </a>
+      </li>
+    </ol>
+  </div>
+
+
+?>
+
+
+<!-- This is the 2- column style -->
+<?php if ($footer_style == "2"):
+
+  <!-- First footer column -->
+  <div class="col-sm-12 col-lg-3">
+    <ol>
+      <li>
+        <a href="<?php echo variable_get('c_1_1') ? >" target="_blank" > C-1-1 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_2') ? >" target="_blank" > C-1-2 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_3') ? >" target="_blank" > C-1-3 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_4') ? >" target="_blank" > C-1-4 </a>
+      </li>
+    </ol>
+  </div>
+
+  <!-- Second footer column -->
+  <div class="col-sm-12 col-lg-3">
+    <ol>
+      <li>
+        <a href="<?php echo variable_get('c_2_1') ? >" target="_blank" > C-2-1 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_2_2') ? >" target="_blank" > C-2-2 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_2_3') ? >" target="_blank" > C-2-3 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_2_4') ? >" target="_blank" > C-2-4 </a>
+      </li>
+    </ol>
+  </div>
+
+
+?>
+
+
+<!-- This is the single column style -->
+<?php if ($footer_style == "1"):
+
+
+  <!-- First footer column -->
+  <div class="col-sm-12 col-lg-3">
+    <ol>
+      <li>
+        <a href="<?php echo variable_get('c_1_1') ? >" target="_blank" > C-1-1 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_2') ? >" target="_blank" > C-1-2 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_3') ? >" target="_blank" > C-1-3 </a>
+      </li>
+      <li>
+        <a href="<?php echo variable_get('c_1_4') ? >" target="_blank" > C-1-4 </a>
+      </li>
+    </ol>
+  </div>
+
+?>
+
+</div>
+
+</footer>
+<!-- End of the footer region -->
